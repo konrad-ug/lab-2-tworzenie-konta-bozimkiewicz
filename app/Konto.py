157d3e51ace7d28e5db59bdf2086e84718d95b88
bozimkiewicz <bartek.ozim@wp.pl>
2022-10-23 00:57:43 +0200

    Added promo code handling in Konto.py

--- app/Konto.py
+++ app/Konto.py
@@ -1,15 +1,20 @@
 import re
 
 class Konto:
-    def __init__(self, imie, nazwisko, pesel):
+    def __init__(self, imie, nazwisko, pesel, kupon_rabatowy = None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
         
         self.obsluga_peselu(pesel)
+        self.obsluga_kuponu_rabatowego(kupon_rabatowy)
 
     def obsluga_peselu(self, pesel):
         if len(pesel) == 11:
             self.pesel = pesel
         else:
             self.pesel = "Niepoprawny pesel"
+
+    def obsluga_kuponu_rabatowego(self, kupon):
+        if kupon != None and re.search("PROM_", kupon) != None and len(kupon) == 8:
+            self.saldo = 50
