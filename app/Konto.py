f78c553ff26fec7adb922e77256009e04311a2a7
bozimkiewicz <bartek.ozim@wp.pl>
2022-10-23 00:38:53 +0200

    Update in Konto.py (added pesel)

--- app/Konto.py
+++ app/Konto.py
@@ -1,3 +1,8 @@
+import re
+
 class Konto:
-    def __init__(self):
-        pass
+    def __init__(self, imie, nazwisko, pesel):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.saldo = 0
+        self.pesel = pesel
