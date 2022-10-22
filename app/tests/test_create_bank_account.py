652e844e787fab5765660a8a098b94c3cc34fca2
bozimkiewicz <bartek.ozim@wp.pl>
2022-10-23 00:56:51 +0200

    Added tests for handling promo code

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,6 +8,7 @@ class TestCreateBankAccount(unittest.TestCase):
     imie = "Dariusz"
     nazwisko = "Januszewski"
     pesel = "99123456789"
+    kupon = "PROM_ABC"
 
     def test_tworzenie_konta(self):
         pierwsze_konto = Konto("Dariusz", "Januszewski", "99123456789")
@@ -24,3 +25,12 @@ class TestCreateBankAccount(unittest.TestCase):
         zly_pesel = "123"
         konto = Konto(self.imie, self.nazwisko, zly_pesel)
         self.assertEqual(konto.pesel, "Niepoprawny pesel", "Pesel został podany prawidłowo")
+
+    def test_kupon_rabatowy_prawidlowy(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel, self.kupon)
+        self.assertEqual(konto.saldo, 50, "Saldo nie równa się 50zł")
+
+    def test_kupon_rabatowy_nieprawidlowy(self):
+        zly_kupon = 'abc'
+        konto = Konto(self.imie, self.nazwisko, self.pesel, zly_kupon)
+        self.assertEqual(konto.saldo, 0, "Saldo nie równa się 0zł")
