a60d06526084db257f026d55760e3038bf327638
bozimkiewicz <bartek.ozim@wp.pl>
2022-11-01 20:56:38 +0100

    Added funds transfer handling in Konto.py

--- app/Konto.py
+++ app/Konto.py
@@ -6,7 +6,7 @@ class Konto:
         
         self.obsluga_peselu(pesel)
         if (self.obsluga_kuponu_rabatowego_kupon(kupon_rabatowy) 
-        and self.obsluga_kuponu_rabatowego_wiek(kupon_rabatowy)):
+        and self.obsluga_kuponu_rabatowego_wiek()):
             self.saldo = 50
 
     def obsluga_peselu(self, pesel):
@@ -19,6 +19,16 @@ class Konto:
         if (kupon != None and kupon[:5] == 'PROM_' and len(kupon) == 8):
             return True
         
-    def obsluga_kuponu_rabatowego_wiek(self, kupon):
+    def obsluga_kuponu_rabatowego_wiek(self):
         if (int(self.pesel[2:4]) > 20 or int(self.pesel[:2]) > 60):
             return True
+
+    def zaksieguj_przelew_przychodzacy(self, kwota):
+        self.saldo += kwota
+    
+    def zaksieguj_przelew_wychodzacy(self, kwota):
+        if kwota > self.saldo:
+            return 'Za mało środków na koncie'
+        else:
+            self.saldo -= kwota
+    
