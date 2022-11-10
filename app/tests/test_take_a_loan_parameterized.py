f939a79300fe6e441b20854a4813aac2e22cd374
bozimkiewicz <bartek.ozim@wp.pl>
2022-11-10 21:35:34 +0100

    Added parameterized tests for take a loan method

---
+++ app/tests/test_take_a_loan_parameterized.py
@@ -0,0 +1,50 @@
+import unittest
+from parameterized import parameterized
+
+from ..Konto import Konto
+
+class TestTakeALoanParameterized(unittest.TestCase):
+    imie = "Dariusz"
+    nazwisko = "Januszewski"
+    pesel = "99123456789"
+
+    def setUp(self):
+        self.konto = Konto(self.imie, self.nazwisko, self.pesel)
+
+    @parameterized.expand([
+        ([], False),
+        ([500, 500, 500], True),
+        ([500, -500, 500], False),
+
+    ])
+    def test_zaciagnij_kredyt_warunek_pierwszy(self, historia, wynik_wniosku):
+        self.konto.historia = historia
+        czy_przyznano = self.konto.check_zaciagnij_kredyt_ostatnie_transakcje_wplaty()
+
+        self.assertEqual(czy_przyznano, wynik_wniosku)
+
+    @parameterized.expand([
+        ([], 1000, False),
+        ([-100, 500, 500, -300, 500], 1000, True),
+        ([-250, 500, 500, -300, 500], 1000, False)
+    ])
+    def test_zaciagnij_kredyt_warunek_drugi(self, historia, kwota, wynik_wniosku):
+        self.konto.historia = historia
+        czy_przyznano = self.konto.check_zaciagnij_kredyt_suma_transakcji(kwota)
+        
+        self.assertEqual(czy_przyznano, wynik_wniosku)
+
+    @parameterized.expand([
+        ([], 1000, 0),
+        ([500, 500, 500], 1000, 1000),
+        ([-100, 500, 500, -300, 500], 1000, 1000),
+        ([500, -500, 500], 1000, 0),
+        ([-250, 500, 500, -300, 500], 1000, 0)
+    ])
+    def test_zaciagnij_kredyt_kwota(self, historia, kwota, saldo):
+        self.konto.historia = historia
+
+        self.konto.zaciagnij_kredyt(kwota)
+
+        self.assertEqual(self.konto.saldo, saldo)
+    
