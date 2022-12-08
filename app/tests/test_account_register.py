60e1f520f24714110ba533ddfc26120c75c7fce5
bozimkiewicz <bartek.ozim@wp.pl>
2022-12-08 19:15:06 +0100

    Refactor

--- app/tests/test_account_register.py
+++ app/tests/test_account_register.py
@@ -5,21 +5,24 @@ from ..Konto import Konto
 
 class TestAccountRegister(unittest.TestCase):
 
-    imie = "Dariusz"
-    nazwisko = "Januszewski"
-    pesel = "99123456789"
+    @classmethod
+    def setUpClass(cls):
+        cls.konto_pierwsze = Konto("Dariusz", "Januszewski", "99123456789")
+        cls.konto_drugie = Konto("Tomasz", "Nowak", "98123456789")
+        cls.konto_trzecie = Konto("Adam", "Kowalski", "97123456789")
 
-    def test_1_dodanie_konta(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        RejestrKont.dodaj_konto(konto)
-        self.assertEqual(RejestrKont.zwroc_ilosc_kont(), 1)
+    def test_1_dodanie_konta(cls):
+        RejestrKont.dodaj_konto(cls.konto_pierwsze)
+        cls.assertEqual(RejestrKont.zwroc_ilosc_kont(), 1)
 
-    def test_2_dodanie_konta_drugie(self):
-        konto_drugie = Konto(self.imie, self.nazwisko, self.pesel)
-        RejestrKont.dodaj_konto(konto_drugie)
-        self.assertEqual(RejestrKont.zwroc_ilosc_kont(), 2)
+    def test_2_dodanie_konta_drugie(cls):
+        RejestrKont.dodaj_konto(cls.konto_drugie)
+        cls.assertEqual(RejestrKont.zwroc_ilosc_kont(), 2)
 
-    def test_3_wyszukaj_konto(self):
-        konto_trzecie = Konto(self.imie, self.nazwisko, "98123456789")
-        RejestrKont.dodaj_konto(konto_trzecie)
-        self.assertEqual(RejestrKont.wyszukaj_konto_osobiste("98123456789"), konto_trzecie)
+    def test_3_wyszukaj_konto(cls):
+        RejestrKont.dodaj_konto(cls.konto_trzecie)
+        cls.assertEqual(RejestrKont.wyszukaj_konto_osobiste("97123456789"), cls.konto_trzecie)
+
+    @classmethod
+    def tearDownClass(cls):
+        RejestrKont.konta = []
